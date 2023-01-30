68592f49e6b841027e121b12bf1b26b190583940
Martin Ferreira <martin.f.yic@gmail.com>
2023-01-29 21:44:06 -0300

    feat: :fire: remove child_process

--- src/routes/randoms.js
+++ src/routes/randoms.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
-const { fork } = require('node:child_process');
 const { loggerInfo, loggerError } = require('../middlewares/log4js.js');
+const randomNumbers = require('../utils/randomsNumber');
 
 const randomRoute = Router();
 
@@ -8,16 +8,8 @@ randomRoute.get('/', (req, res) => {
 	try {
 		loggerInfo.info('Se accedió a /randoms');
 		const cant = req.query.cant || 100000000;
-		const child_process = fork('./src/utils/randomsNumber.js');
 
-		child_process.send(cant);
-		child_process.on('message', msg => {
-			res.render('randomsNumbers', { msg });
-		});
-
-		child_process.on('exit', code => {
-			console.log('Se ha cerrado el proceso', code);
-		});
+		res.render('randomsNumbers', { msg: randomNumbers(cant) });
 	} catch (error) {
 		loggerError.error(`Error en /randoms ==> ${error}`);
 		res.send('Error');
