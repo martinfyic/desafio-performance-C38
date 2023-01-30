f9fb4cf7095c7d73eefc6bdc8d71b4fbee0e29c3
Martin Ferreira <martin.f.yic@gmail.com>
2023-01-29 21:27:47 -0300

    feat: :zap: Add try catch

--- src/routes/404.js
+++ src/routes/404.js
@@ -4,8 +4,13 @@ const { loggerWarn } = require('../middlewares/log4js.js');
 const error404 = Router();
 
 error404.get('/', (req, res) => {
-	loggerWarn.warn('404 | Ruta no encontrada');
-	res.render('404');
+	try {
+		loggerWarn.warn('404 | Ruta no encontrada');
+		res.render('404');
+	} catch (error) {
+		loggerError.error(`Error en 404 ==> ${error}`);
+		res.send('Error');
+	}
 });
 
 module.exports = error404;
