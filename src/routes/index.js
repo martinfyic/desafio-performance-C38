49fe5acf52f455e3994342b2bfabfb61cc10815f
Martin Ferreira <martin.f.yic@gmail.com>
2023-01-29 21:10:17 -0300

    chore: :wrench: Add error404 in index
    Add 404.js in index to export barrel

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const ecommerceRoute = require('./ecommerce.js');
 const infoRouter = require('./info.js');
 const randomRoute = require('./randoms.js');
+const error404 = require('./404.js');
 
 module.exports = {
 	ecommerceRoute,
 	infoRouter,
 	randomRoute,
+	error404,
 };
